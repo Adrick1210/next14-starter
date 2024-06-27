bb1f2409e495229181aa2d4b74dccade946c09f1
Christian Alexander <christian.alexanderato@gmail.com>
2024-06-26 23:12:08 -0700

    more style practice

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,10 @@
+import styles from "./footer.module.css"
+
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className={styles.container}>
+     <div className={styles.logo}>Logo</div>
+     <div className={styles.text}>Creative thoughts agency &copy; All rights reserved </div>
     </div>
   )
 }
