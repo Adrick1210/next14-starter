4d16d81c2497a9df4cf82c3de2fe8003d51d0e94
Christian Alexander <christian.alexanderato@gmail.com>
2024-06-30 19:16:51 -0700

    SEO tutorial

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -14,27 +14,35 @@ import { Post } from "@/lib/models";
 //   return res.json();
 // };
 
+export const generateMetadata = async ({ params }) => {
+  const { slug } = params;
+
+  const post = await getPost(slug);
+
+  return {
+    title: post.title,
+    description: post.desc,
+  };
+};
+
 const SinglePostPage = async ({ params }) => {
   const { slug } = params;
 
   // const post = await getData(slug);
+
   const post = await getPost(slug);
 
-  console.log(post)
+  console.log(post);
   return (
     <div className={styles.container}>
-      {post.img && <div className={styles.imgContainer}>
-        <Image
-          className={styles.img}
-          src={post.img}
-          alt=""
-          fill
-        />
-      </div>}
+      {post.img && (
+        <div className={styles.imgContainer}>
+          <Image className={styles.img} src={post.img} alt="" fill />
+        </div>
+      )}
       <div className={styles.textContainer}>
         <h1 className={styles.title}>{post.title}</h1>
         <div className={styles.detail}>
-          
           {post && (
             <Suspense fallback={<div>Loading...</div>}>
               <PostUser userId={post.userId} />
@@ -42,12 +50,12 @@ const SinglePostPage = async ({ params }) => {
           )}
           <div className={styles.detailText}>
             <span className={styles.detailTitle}>Published</span>
-            <span className={styles.detailValue}>{post.createdAt.toString().slice(4,16)}</span>
+            <span className={styles.detailValue}>
+              {post.createdAt.toString().slice(4, 16)}
+            </span>
           </div>
         </div>
-        <div className={styles.content}>
-          {post.desc}
-        </div>
+        <div className={styles.content}>{post.desc}</div>
       </div>
     </div>
   );
